b0d989771a5be77df2e648b2e4e3983a65edbcad
DaniloDev <daniloemanuel684@gmail.com>
2026-04-09 13:41:58 -0300

    Design Pattern: Proxy for database access

--- src/crud.py
+++ src/crud.py
@@ -1,45 +1,25 @@
 import sqlite3
+from src.dbmanager import DbManager
 
 class CRUD:
     def __init__(self):
-        self.conn = sqlite3.connect('crud.db')
-        self.cursor = self.conn.cursor()
-        self.create_table()
-
-    def create_table(self):
-        self.cursor.execute('''create table if not exists users (
-                                    id integer primary key autoincrement,
-                                    name text,
-                                    profession text,
-                                    payment real
-                                    )''')
+        self.dbmanager = DbManager()
 
     def create(self, name: str, profession: str, payment: float):
-        self.cursor.execute('insert into users (name, profession, payment) values (?, ?, ?)', (name, profession, payment))
-        self.conn.commit()
-        print('[OK] - Values entered successfully')
+        self.dbmanager.execute("insert into users (name, profession, payment) values (?, ?, ?)",
+                               (name, profession, payment))
+        print("[OK] - Values entered successfully")
 
     def read(self):
-        self.cursor.execute("select * from users")
-        return self.cursor.fetchall()
+        fetch = self.dbmanager.select("select * from users", 1)
+        return fetch
 
     def update(self, row_id: int, column_name: str, new_value: str | float):
-        self.cursor.execute(f'''update users
-                                set {column_name} = ?
-                                where id = ?''', (new_value, row_id))
-        self.conn.commit()
-        print('[OK] - Values updated')
+        self.dbmanager.execute(f'''update users
+                                 set {column_name} = ?
+                                 where id = ?''', (new_value, row_id))
+        print("[OK] - Values updated")
 
     def delete(self, row_id: int):
-        self.cursor.execute(f"delete from users where id = ?",  (row_id,))
-        self.conn.commit()
-        print(f'[OK] - Row {row_id} deleted')
-
-    def is_content(self):
-        self.cursor.execute("select * from users limit 1;")
-        result = self.cursor.fetchone()
-        return result
-
-    def close(self):
-        if self.conn:
-            self.conn.close()
+        self.dbmanager.execute(f"delete from users where id = ?", (row_id,))
+        print(f"[OK] - Row {row_id} deleted")
